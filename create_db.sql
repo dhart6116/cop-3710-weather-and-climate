582ce657758e31dfca56254432180a74ea492220
dhart6116 <dhart6116@floridapoly.edu>
2026-03-27 08:48:07 -0400

    Create create_db.sql

---
+++ create_db.sql
@@ -0,0 +1,24 @@
+CREATE TABLE city_attributes (
+	CityID INTEGER PRIMARY KEY,
+	Latitude FLOAT,
+	Longitude FLOAT,
+	CityName VARCHAR(20),
+	Country VARCHAR(20)
+);
+
+CREATE TABLE records (
+	RecordID INTEGER PRIMARY KEY,
+	CityID INTEGER,
+	DateDay DATE,
+	DateHour INTEGER
+);
+
+CREATE TABLE weather_desc_lookup (
+	DescID INTEGER PRIMARY KEY,
+	Description VARCHAR(30)
+);
+
+CREATE TABLE humidity (
+	RecordID INTEGER PRIMARY KEY,
+	Humidity FLOAT
+);
