7430318af0257af53824e614f5aad5707ea71b77
dhart6116 <dhart6116@floridapoly.edu>
2026-03-29 14:29:21 -0400

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -1,45 +1,88 @@
 CREATE TABLE city_attributes (
-	CityID INT PRIMARY KEY,
-	Latitude FLOAT(24),
-	Longitude FLOAT(24),
+	CityID INT,
+	Latitude FLOAT,
+	Longitude FLOAT,
 	CityName VARCHAR(20),
-	Country VARCHAR(20)
+	Country VARCHAR(20),
+	PRIMARY KEY (CityID)
 );
 
 CREATE TABLE records (
-	RecordID INT PRIMARY KEY,
-	CityID INT,
-	DateDay DATE,
-	DateHour INT
+	RecordID INT,
+	CityID INT NOT NULL,
+	DateDay DATE NOT NULL,
+	DateHour INT NOT NULL,
+	PRIMARY KEY (RecordID)
 );
 
 CREATE TABLE weather_desc_lookup (
-	DescID INT PRIMARY KEY,
-	Description VARCHAR(30)
+	DescID INT,
+	Description VARCHAR(30),
+	PRIMARY KEY (DescID)
 );
 
 CREATE TABLE humidity (
-	RecordID INT PRIMARY KEY,
-	Humidity FLOAT(24)
+	RecordID INT,
+	Humidity FLOAT,
+	PRIMARY KEY (RecordID)
 );
 
 CREATE TABLE pressure (
-	RecordID INT PRIMARY KEY,
-	Pressure FLOAT(24)
+	RecordID INT,
+	Pressure FLOAT,
+	PRIMARY KEY (RecordID)
 );
 
 CREATE TABLE temperature (
-	RecordID INT PRIMARY KEY,
-	Temperature FLOAT(24)
+	RecordID INT,
+	Temperature FLOAT,
+	PRIMARY KEY (RecordID)
 );
 
 CREATE TABLE wind (
-	RecordID INT PRIMARY KEY,
-	WindDir FLOAT(24),
-	WindSpd FLOAT(24)
+	RecordID INT,
+	WindDir FLOAT,
+	WindSpd FLOAT,
+	PRIMARY KEY (RecordID)
 );
 
 CREATE TABLE weather_description (
-	RecordID INT PRIMARY KEY,
-	DescID INT
+	RecordID INT,
+	DescID INT,
+	PRIMARY KEY (RecordID)
 );
+
+ALTER TABLE records
+ADD CONSTRAINT fk_records_city
+	FOREIGN KEY (CityID)
+	REFERENCES city_attributes(CityID);
+	
+ALTER TABLE humidity
+ADD CONSTRAINT fk_humidity_records
+	FOREIGN KEY (RecordID)
+	REFERENCES records(RecordID);
+	
+ALTER TABLE pressure
+ADD CONSTRAINT fk_pressure_records
+	FOREIGN KEY (RecordID)
+	REFERENCES records(RecordID);
+	
+ALTER TABLE temperature
+ADD CONSTRAINT fk_temp_records
+	FOREIGN KEY (RecordID)
+	REFERENCES records(RecordID);
+	
+ALTER TABLE wind
+ADD CONSTRAINT fk_wind_records
+	FOREIGN KEY (RecordID)
+	REFERENCES records(RecordID);
+	
+ALTER TABLE weather_description
+ADD CONSTRAINT fk_wdesc_records
+	FOREIGN KEY (RecordID)
+	REFERENCES records(RecordID);
+
+ALTER TABLE weather_description
+ADD CONSTRAINT fk_wdesc_desclookup
+	FOREIGN KEY (DescID)
+	REFERENCES weather_desc_lookup(DescID);
